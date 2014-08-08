90c78b56eefa53877e06951b0d599b43d836b61f
plsql <master@branch>
2014-08-08 12:24:55 -0400

    adding song-dupe.py

---
+++ song-dupe.py
@@ -0,0 +1,48 @@
+# A simple script to remove iTunes' irritating duplicated files.
+
+import os, hashlib
+from collections import defaultdict
+
+print "enter root directory under which all music is stored: "
+music_dir = raw_input()
+
+song_paths = set()
+
+for (dirpath, _, filenames) in os.walk(music_dir):
+    for filename in filenames:
+        if any([filename.strip().endswith(suffix) for suffix in [".mp3", ".flac", ".wav", ".aac"]]):
+            song_paths.add(dirpath + '/' + filename)
+
+print len(song_paths), "song paths gathered"
+
+
+proc_count = 0
+checksums = defaultdict(list)
+
+for song_path in song_paths:
+
+    if proc_count % 1000 == 0:
+        print proc_count / 1000, "thousand files hashed"
+    proc_count += 1
+
+    with open(song_path, "r") as song_file:
+        md5 = hashlib.new("md5")
+        md5.update(song_file.read())
+        checksums[md5.digest()].append(song_path)
+
+
+for dupe_paths in checksums.values():
+
+    if len(dupe_paths) > 1:
+        dupe_paths.sort()
+        for dupe_path in dupe_paths[1:]:
+            if dupe_path.strip().split()[-1].isdigit() and dupe_path.startswith(dupe_paths[0]):
+                print "will delete", dupe_path
+
+
+with open("dupefiles.txt", "w") as outfile:
+    lines = []
+    for dupe_paths in checksums.values():
+        if len(dupe_paths) > 1:
+            lines.append('\n'.join(dupe_paths) + '\n\n')
+    outfile.writelines(lines)
